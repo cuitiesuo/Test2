86362f17512b720013eecd967cad2909a8e31152
cuitiesuo <cuitiesuo@163.com>
2017-07-27 10:01:31 +0800

    新增11111

--- src/main/java/com/example/demo/Test123Application.java
+++ src/main/java/com/example/demo/Test123Application.java
@@ -8,5 +8,6 @@ public class Test123Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test123Application.class, args);
+		System.out.println("111111111111111");
 	}
 }
