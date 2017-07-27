7902fc4993fb8156d4fe0f8ee172524b5ba17bb1
cuitiesuo <cuitiesuo@163.com>
2017-07-27 10:03:14 +0800

    新增打印2

--- src/main/java/com/example/demo/Test123Application.java
+++ src/main/java/com/example/demo/Test123Application.java
@@ -9,5 +9,6 @@ public class Test123Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Test123Application.class, args);
 		System.out.println("111111111111111");
+		System.out.println("2");
 	}
 }
