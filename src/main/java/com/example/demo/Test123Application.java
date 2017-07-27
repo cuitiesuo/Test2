cb0904fcbc191e696cfeb415a133076af56272a4
cuitiesuo <cuitiesuo@163.com>
2017-07-27 15:15:12 +0800

    冲突好烦啊

--- src/main/java/com/example/demo/Test123Application.java
+++ src/main/java/com/example/demo/Test123Application.java
@@ -29,8 +29,10 @@ public class Test123Application {
 		System.out.println("我还是数字 ，该出通途了");
 		System.out.println("2222");
 		System.out.println("我还是1");
+		System.out.println("你还来");
 		System.out.println("再来一次");
 		System.out.println("你还来");
+		System.out.println("冲突好烦啊");
 
 	}
 }
